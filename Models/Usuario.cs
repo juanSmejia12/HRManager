f1bbda9c6f787a27fb7ab0ecdbc3ca60f339294c
JheisonGarcia1912 <castillojheison44@gmail.com>
2025-05-27 16:18:12 -0500

    Created Usuario model

---
+++ Models/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRManager.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int IdUsuario { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string NombreUsuario { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string PasswordHash { get; set; }
+
+        // Podemos agregar más propiedades aquí, como Rol, FechaCreacion, etc.
+    }
+}
