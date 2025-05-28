bd05d8f67222c11dd54cb87982e95d679ac1e920
JheisonGarcia1912 <castillojheison44@gmail.com>
2025-05-27 19:17:39 -0500

    Eliminar

---
+++ Pages/Usuario/Eliminar.cshtml.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using HRManager.Models;
+using HRManager.Data;
+
+namespace HRManager.Pages.Usuario
+{
+    public class EliminarModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EliminarModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Usuarios Usuario { get; set; } // Usa Usuarios
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Usuario = await _context.Usuarios.FirstOrDefaultAsync(m => m.IdUsuario == id);
+
+            if (Usuario == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Usuario = await _context.Usuarios.FindAsync(id);
+
+            if (Usuario != null)
+            {
+                _context.Usuarios.Remove(Usuario);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
