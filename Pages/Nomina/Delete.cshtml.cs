8d57f4993d80e5cee9d97d92e1497161e435949a
CristianBed <162656608+CristianBed@users.noreply.github.com>
2025-05-29 09:42:49 -0500

    configurando nomina delete

---
+++ Pages/Nomina/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HRManager.Pages.Nomina
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
